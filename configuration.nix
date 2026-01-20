ffd20ab267d79be728a459dc71cf2d8c33f4c225
jimmycanosa <44603136+jimmycanosa@users.noreply.github.com>
2026-01-20 06:10:40 +0000

    Add support for NTFS filesystems and blacklist ntfs3 kernel module

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,8 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "ntfs" ];
+  boot.blacklistedKernelModules = [ "ntfs3" ];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
